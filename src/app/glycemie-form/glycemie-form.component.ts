15469c3e1df1b42b315cb3f15e9c838421fa0636
iima23 <elmazouz.imane@gmail.com>
2024-06-28 15:55:02 +0100

    add function addGlycemie

--- src/app/glycemie-form/glycemie-form.component.ts
+++ src/app/glycemie-form/glycemie-form.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule} from "@angular/forms";
+import {ActivatedRoute, Router, RouterOutlet} from '@angular/router';
+import { GlycemieService } from '../glycemie-service.service';
+import { Glycemie } from '../../model/glycemie';
 
 @Component({
   selector: 'app-glycemie-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterOutlet],
   templateUrl: './glycemie-form.component.html',
   styleUrl: './glycemie-form.component.css'
 })
 export class GlycemieFormComponent {
+  glycemie : Glycemie;
 
+
+  constructor(
+    private route : ActivatedRoute,
+    private router : Router,
+    private glycemieService : GlycemieService
+  ) {
+    this.glycemie = new Glycemie();
+  }
+
+
+onSubmit() {
+  this.glycemieService.save(this.glycemie).subscribe(result => this.gotoGlycemieList());
+}
+
+  gotoGlycemieList(){
+    this.router.navigate(['/glycemies']);
+  }
 }
