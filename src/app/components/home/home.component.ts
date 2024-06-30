5f1e6ef935f086ea63faca974b6662f34017b83f
iima23 <elmazouz.imane@gmail.com>
2024-06-30 21:59:57 +0100

    edit front end

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {RouterLink} from "@angular/router";
 
+
 @Component({
   selector: 'app-home',
   standalone: true,
